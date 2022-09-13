27e253ed733c30c5b1bd816719078aa08685234b
Akanji Ayomide Gideon <110993463+Ayomidegideon123@users.noreply.github.com>
2022-09-13 00:31:18 -0700

    Update 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,1 +1,15 @@
-go
+#include "main.h"
+
+/**
+ * add - adds two integers
+ *
+ * @a: number
+ * @b: number
+ *
+ * Return: the sum of two integers
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
